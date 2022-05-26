923809b25372ea19d10396b1b848743108e2b49f
FayJu <faizan@unetiq.com>
2022-05-26 17:58:18 +0200

    Added Register

---
+++ src/views/RegisterUser.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-form v-model="valid">
+        <v-container>
+            <v-row>
+                <v-col cols="12" md="4">
+                    <v-text-field v-model="name" :rules="nameRules" label="Name" required></v-text-field>
+                </v-col>
+
+                <v-col cols="12" md="4">
+                    <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-form>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            valid: false,
+            name: '',
+            nameRules: [(v) => !!v || 'Name is required'],
+            email: '',
+            emailRules: [(v) => !!v || 'E-mail is required', (v) => /.+@.+/.test(v) || 'E-mail must be valid'],
+        };
+    },
+};
+</script>
+<style></style>
